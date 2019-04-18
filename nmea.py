980959f1a2bd6df38e020516f0dc332d8b7ef313
mitxela <mitxela@mitxela.com>
2019-04-18 21:52:29 +0100

    Python script for generating fake NMEA strings
    Useful for debugging, but also a fully functional means of setting the
    clock by NTP. Uses RTS to simulate PPS signal.

---
+++ nmea.py
@@ -0,0 +1,33 @@
+import datetime
+import sys
+import time
+import iso8601
+import serial
+
+ONE_SEC = datetime.timedelta(seconds=1)
+
+def format_gprmc(dt):
+	return "$GPRMC,{}.000,A,5321.6802,N,00630.3372,W,0.02,31.66,{},,,A*43\n".format(
+		dt.strftime('%H%M%S'),
+		dt.strftime('%d%m%y')
+	).encode('ascii')
+
+with serial.Serial('/dev/ttyUSB0', 9600) as ser:
+	ser.rts = True
+
+	if len(sys.argv) >= 2:
+		start_at = iso8601.parse_date(sys.argv[1])
+	else:
+		start_at = datetime.datetime.utcnow()
+
+	while True:
+		now = datetime.datetime.utcnow()
+		us = now.microsecond
+		time.sleep((1000000 - us) / 1000000)
+		ser.rts = False
+		time.sleep(0.1)
+		ser.rts = True
+
+		start_at += ONE_SEC
+		ser.write(format_gprmc(start_at))
+
