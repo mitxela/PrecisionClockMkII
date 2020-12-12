97fbb9cf882cbda4d9f8143bc6f371396ee0a4e2
mitxela <mitxela@mitxela.com>
2020-12-12 01:06:23 +0000

    Output correct checksum in nmea.py

--- nmea.py
+++ nmea.py
@@ -7,12 +7,17 @@ import serial
 ONE_SEC = datetime.timedelta(seconds=1)
 
 def format_gprmc(dt):
-	return "$GPRMC,{}.000,A,5321.6802,N,00630.3372,W,0.02,31.66,{},,,A*43\n".format(
+	str= "GPRMC,{}.000,A,5321.6802,N,00630.3372,W,0.02,31.66,{},,,A".format(
 		dt.strftime('%H%M%S'),
 		dt.strftime('%d%m%y')
-	).encode('ascii')
+	)
 
-with serial.Serial('/dev/ttyUSB0', 9600) as ser:
+	checksum = 0
+	for c in str:
+		checksum ^= ord(c)
+	return ("$%s*%02X\n" % (str, checksum)).encode('ascii')
+
+with serial.Serial('/dev/ttyUSB', 9600) as ser:
 	ser.rts = True
 
 	if len(sys.argv) >= 2:
